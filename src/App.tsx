49e16f9fd1df02f95efddc0545e51f822ee4b947
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2022-04-30 12:37:38 -0300

    feat: create a html and css of login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import { Input } from '@chakra-ui/react'
+import { Button, Container, Input } from '@chakra-ui/react'
 
 function App() {
-  return <Input />
+  return (
+    <Container
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+    >
+      <Input marginBottom="0.6rem" placeholder="email@mail.com" />
+      <Input marginBottom="0.6rem" placeholder="*********" />
+      <Button width="100%" colorScheme="cyan">
+        Login
+      </Button>
+    </Container>
+  )
 }
 
 export default App
