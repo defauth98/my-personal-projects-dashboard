66ce2180f397943053a52fd94692169e3c835f6e
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2022-05-21 02:25:44 -0300

    feat: create check to verify if user keep session

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, Input } from '@chakra-ui/react'
+import { Button, Container, Input, Checkbox, Box } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
 
 import { useAuth } from '../contexts/authContext'
@@ -6,6 +6,7 @@ import { useAuth } from '../contexts/authContext'
 function LoginPage() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [keepSession, setKeepSession] = useState(false)
 
   const { user, handleLogin, retrieveDataFromLocalStorage } = useAuth()
 
@@ -36,10 +37,20 @@ function LoginPage() {
         type="password"
         onChange={(event) => setPassword(event.target.value)}
       />
+      <Box>
+        <Checkbox
+          marginBottom="0.6rem"
+          marginRight="0.6rem"
+          type="checkbox"
+          checked={keepSession}
+          onChange={() => setKeepSession(!keepSession)}
+        />
+        Continuar logado
+      </Box>
       <Button
         width="100%"
         colorScheme="cyan"
-        onClick={() => handleLogin(email, password, true)}
+        onClick={() => handleLogin(email, password, keepSession)}
       >
         Login
       </Button>
