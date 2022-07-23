add6c23d94ab40a9dbb1a46a1fdc3b682a4b28f2
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2022-07-23 13:56:37 -0300

    fix: loading

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -33,7 +33,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData)
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null)
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
   const toast = useToast()
 
   const navigation = useNavigate()
